ea31868842cfa361314dfd75eab9506c3c652f4e
Ekkachai Jet Ittihrit <jet@Ekkachais-MacBook-Pro.local>
2020-12-06 08:08:08 -0800

    //index js update with test

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+//Test code to change the background color of the border.
+var test=document.getElementsByClassName('selection');
+
+
+for (i = 0; i < test.length; i++) {
+    test[i].addEventListener("click", function(){
+      document.getElementById("borderImageID").style.backgroundColor = "black";
+    })
+}
+
+
