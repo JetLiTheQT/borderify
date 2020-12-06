b2da72379026a7fdc57de1e3cb601d3b9f6a4dcb
Max Taylor <max.taylor12@yahoo.com>
2020-12-06 07:48:57 -0800

    background css gradient

---
+++ public/index.js
@@ -0,0 +1,14 @@
+<script>
+    var count = 1;
+    function setColor(btn, color) {
+        var property = document.getElementById(btn);
+        if (count == 0) {
+            property.style.backgroundColor = "#FFFFFF"
+            count = 1;        
+        }
+        else {
+            property.style.backgroundColor = "#7FFF00"
+            count = 0;
+        }
+    }
+</script>
