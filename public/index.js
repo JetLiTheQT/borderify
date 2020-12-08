4332131b6d234dc3e8ed8481aa0b56d911d6fd07
Max Taylor <max.taylor12@yahoo.com>
2020-12-07 19:08:51 -0800

    changed tabs to js

--- public/index.js
+++ public/index.js
@@ -11,6 +11,10 @@ function getBtnInfo(){
 }
 */
 
+//code to make the colors tab the default selected tab
+//document.getElementById("defaultOpen").click();
+
+
 //Function to insert a new button.
 function insertNewButton(color, id/*imageURL*/){
   var context = {
@@ -42,6 +46,8 @@ function loadButtons(){
 
 
 
+
+
 /* Function for Input for image-only files.
    GOAL: Take that input, store it in index.js and create a new dom where borderImage should be. 
     - Make sure to set width to 100% to fill the borderImage container */
@@ -53,3 +59,25 @@ function loadButtons(){
 //GOAL 
 //Make the website automatically load a random image/color when initially loaded so the background-color of the borderImageContainer & borderImage aren't the same
 window.addEventListener("load", loadButtons);
+
+//Tabs functions
+function openTab(evt, tabCorI) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabCorI).style.display = "block";
+  evt.currentTarget.className += " active";
+}
